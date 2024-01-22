1e5a0162a8548799f3f8d3e2d6e9d8c61bdb2a08
GeorgStrassberger <georg.strassberger@googlemail.com>
2024-01-22 21:29:07 +0100

    change naming

--- index.php
+++ index.php
@@ -3,14 +3,16 @@
 require_once __DIR__ . '/inc/all.php';
 
 
-$page = @(string) ($_GET['page'] ?? 'index'); // $page = isset($_GET['page']) ? $_GET['page'] : 'index';
+$route = @(string) ($_GET['page'] ?? 'page'); // $page = isset($_GET['page']) ? $_GET['page'] : 'index';
 
 
-if ($page === 'index'){
+if ($route === 'page'){
     $pagesContoller = new App\Controller\PagesController(
         new \App\Pages\PagesRepository()
     );
-    $pagesContoller->showPage('index');
+
+    $page = @(string) ($_GET['page'] ?? 'index');
+    $pagesContoller->showPage($page);
 }else{
     // var_dump("Hier gebe die Fehlerseite aus (Error 404)");
     $notFoundController = new \App\Controller\NotFoundController();
